6f80a67575b959b97ff297d64bffd7af0d7d8504
Atilay <atilaybck@gmail.com>
2024-05-03 20:45:59 +0300

    Search Bar eklendi ve proje tamamlandı.

--- App.js
+++ App.js
@@ -1,38 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, StyleSheet, View, Text, FlatList } from "react-native";
 import music_data from "./src/music-data.json";
 import SongCard from "./src/components/SongCard/SongCard";
+import SearchBar from "./src/components/SearchBar/SearchBar";
 
 function App() {
+  const [list, setList] = useState(music_data); 
 
-  const renderSong  =({ item }) => <SongCard  song ={item}/>
-  
-  const renderSeperator = () => <View style={styles.seperator} />
+  const renderSong = ({ item }) => <SongCard song={item} />;
 
+  const renderSeparator = () => <View style={styles.separator} />;
+
+  const handleSearch = (text) => {
+    const filteredList = music_data.filter((song) => {
+      const searchedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
+
+      return currentTitle.indexOf(searchedText) > -1;
+    });
+
+    setList(filteredList); 
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <FlatList
-          data={music_data}
-          keyExtractor={(item) => item.id}
-          renderItem={renderSong}
-          ItemSeparatorComponent={renderSeperator}
-        />
-      </View>
+      <SearchBar onSearch={handleSearch} />
+      <FlatList
+        data={list} // list durumu kullanıldı
+        keyExtractor={(item) => item.id}
+        renderItem={renderSong}
+        ItemSeparatorComponent={renderSeparator}
+      />
     </SafeAreaView>
   );
-};
+}
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop:15
+    marginTop: 25,
   },
-  seperator: {
-    borderWidth:0.5,
-    borderColor: "#e0e0e0"
-  }
-})
+  separator: {
+    borderWidth: 0.5,
+    borderColor: "#e0e0e0",
+  },
+});
---
+++ src/components/SearchBar/SearchBar.styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: "#eceff1",
+        padding: 8,
+        margin:8,
+        marginTop:10,
+        marginBottom:5,
+        borderRadius:6
+    }
+})
